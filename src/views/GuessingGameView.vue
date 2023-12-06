9fcd21cdea866c8d4daf0a8f405e8fcd90008a95
Austin Sun <austinsun@Austins-MacBook-Pro.local>
2023-12-06 11:32:01 -0800

    initial commit v1

---
+++ src/views/GuessingGameView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    Test
+  </div>
+  <div>
+    <GameBoard />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import GameBoard from '@/components/GameBoard.vue';
+
+@Options({
+  components: {
+    GameBoard,
+  },
+})
+export default class GuessingGameView extends Vue { }
+</script>
